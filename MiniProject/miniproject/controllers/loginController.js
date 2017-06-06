d849829990cf6fdd9bfcd499bb5ea0d68ca02bdc
ranjith-gurrapu <ranjithkumar.gurrapu@gmail.com>
2017-06-06 11:01:09 +0530

    Add files via upload

---
+++ MiniProject/miniproject/controllers/loginController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by nareshit on 06-06-2017.
+ */
+
+app.comtroller("loginController",loginController);
+
+loginController.$inject=["$scope"];
+
+function loginController($scope)
+
+{
+    $scope.login={};
+}
